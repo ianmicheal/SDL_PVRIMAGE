dad30bcdc423db04528fabd5cac62e4e0a4a3f08
Ian Micheal <59771322+ianmicheal@users.noreply.github.com>
2020-08-25 10:41:10 -0400

    Add files via upload

---
+++ SDL_PVREXAMPLE/main.c
@@ -0,0 +1,28 @@
+
+
+//Example BY Ian Micheal 2020
+#include <SDL.h>    
+#include <SDL_image.h> 
+#include <stdlib.h> 
+
+
+int main(int argc, char *argv[])   { 
+   Uint32 flags = SDL_SWSURFACE|SDL_FULLSCREEN; 
+   if(SDL_Init(SDL_INIT_VIDEO) < 0)   { 
+      return -1; 
+   } 
+
+SDL_Surface * screen; 
+SDL_Surface * image; 
+
+screen = SDL_SetVideoMode(640, 480, 16, flags); 
+image  =IMG_Load("/cd/image.pvr");  //load PVR power of 2* example 256x256 512x512 etc using SDL
+//image  = SDL_LoadBMP("/cd/image.bmp");  //load bmp using SDL
+//image  = IMG_Load("image.jpg"); //load jpg  using SDL_image
+
+/* Puting image on the screen up*/ 
+SDL_BlitSurface(image, NULL, screen, NULL); 
+ SDL_Flip(screen);
+ SDL_Delay(10000);
+ 
+ }
