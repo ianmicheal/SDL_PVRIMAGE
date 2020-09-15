7a94ef1a5bec4db64c1203cacd6de49372290e1e
Ian Micheal <59771322+ianmicheal@users.noreply.github.com>
2020-09-15 00:02:39 -0400

    Add files via upload

--- SDL_PVREXAMPLE/main.c
+++ SDL_PVREXAMPLE/main.c
@@ -1,11 +1,13 @@
 
 
 //Example BY Ian Micheal 2020
+#include <kos.h>
 #include <SDL.h>    
 #include <SDL_image.h> 
 #include <stdlib.h> 
 
-
+extern uint8 romdisk[];
+KOS_INIT_ROMDISK(romdisk);
 int main(int argc, char *argv[])   { 
    Uint32 flags = SDL_SWSURFACE|SDL_FULLSCREEN; 
    if(SDL_Init(SDL_INIT_VIDEO) < 0)   { 
@@ -16,7 +18,8 @@ SDL_Surface * screen;
 SDL_Surface * image; 
 
 screen = SDL_SetVideoMode(640, 480, 16, flags); 
-image  =IMG_Load("/cd/image.pvr");  //load PVR power of 2* example 256x256 512x512 etc using SDL
+//image  =IMG_Load("/cd/image.pvr");  //load PVR power of 2* example 256x256 512x512 etc using SDL
+image  =IMG_Load("/rd/image.pvr");  //load PVR power of 2* example 256x256 512x512 etc using SDL
 //image  = SDL_LoadBMP("/cd/image.bmp");  //load bmp using SDL
 //image  = IMG_Load("image.jpg"); //load jpg  using SDL_image
 
